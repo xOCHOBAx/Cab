9bc22482240e90a697c1e4f12aaa67a461f8eb92
Andrey Lazarev <drafter221@gmail.com>
2018-05-17 19:14:15 +0400

    First

---
+++ GetCab/GetCab.CoreLogic/BusinessLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GetCab.BusinessLogicContracts;
+
+namespace GetCab.CoreLogic
+{
+    class BusinessLogic : IBusinessLogic
+    {
+        public bool Add()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GetCab/GetCab.DatabaseDAL/DAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GetCab.DALContracts;
+
+namespace GetCab.DatabaseDAL
+{
+    class DAO : IDAO
+    {
+        public bool Add()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
